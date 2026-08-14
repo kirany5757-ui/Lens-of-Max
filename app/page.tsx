a221b12b8966a1cb0228cfc677898e15b694ea56
kirany5757-ui <kirany5757@gmail.com>
2026-08-14 09:08:05 +0100

    added moody aviation shots

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,8 @@
 import { useState, useEffect, useCallback, useRef } from "react";
 import Image from "next/image";
 const photos = [
+  { id: 20, image: "/Photos/Moody Aviation Shot 2.jpg", story: "", tags: ["Plane", "Aviation", "max", "Edit"], group: "Moody aviation", isMain: true},
+  { id: 21, image: "/Photos/Moody Aviation Shot 1.jpg", story: "", tags: ["Plane", "Aviation", "max", "Edit"], group: "Moody aviation", isMain: false},
   { id: 12, image: "/Photos/Heidi-Love-Baking.jpg", story: "", tags: ["Richmond", "Street", "Bakery", "Edit"], group: "heidi", isMain: true},
   { id: 13, image: "/Photos/Heidi-Love-Baking org.jpg", story: "", tags: ["Richmond", "Street", "Bakery", "Raw"], group: "heidi", isMain: false},
   { id: 17, image: "/Photos/Morning Walk.jpg", story: "", tags: ["Richmond", "Morning walk", "park", "edit"], group: "Morning Walk", isMain: true},
